bd1c6eabc366e0ceae9f4a050a6aff6bfb1f37fe
abdoulaib3 <abdoulaieb3@gmail.com>
2026-09-20 22:30:02 +0100

    nea

--- products-data.js
+++ products-data.js
@@ -6,48 +6,6 @@
    ===================================================================== */
 
 window.VYRON_PRODUCTS = [
-  {
-    "id": "pull",
-    "category": "tshirts",
-    "price": 2000,
-    "images": [
-      "assets/products/image1.jpg"
-    ],
-    "sizes": [
-      "M",
-      "S",
-      "L"
-    ],
-    "colors": [
-      {
-        "hex": "#000000",
-        "name": "noir"
-      },
-      {
-        "hex": "#ffffff",
-        "name": "blanc"
-      }
-    ],
-    "inStock": true,
-    "isNew": true,
-    "isLimited": false,
-    "name": {
-      "fr": "pull",
-      "ar": "pull"
-    },
-    "description": {
-      "fr": "new arrivage",
-      "ar": "new arrivage"
-    },
-    "material": {
-      "fr": "",
-      "ar": ""
-    },
-    "sizeGuideNote": {
-      "fr": "",
-      "ar": ""
-    }
-  }
 ];
 
 window.VYRON_FEATURED = [];
